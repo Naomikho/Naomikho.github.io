776f66daeddeec1368c87a729a1bfea2814ccb41
Naomikho <khoyining9@gmail.com>
2022-06-30 00:38:52 +0800

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 
-function Home(){ 
-        return(
-            <div className = "textAlignCenter vertical-center" centered>
-                <h2>Hi, I'm Naomi, a developer and creator.</h2>
-                <br></br>
-                <h5>You can find my works and portfolio under the works section.</h5>
-                <h5>You can find out more about me on my About page!</h5>
+function Home() {
+    return (
+        <div>
+            <div class="outer">
+                <div class="middle">
+                    <div class="inner">
+                        <div className="textAlignCenter" centered>
+                            <h2>Hi, I'm Naomi, a developer and creator.</h2>
+                            <br></br>
+                            <h5>You can find my works and portfolio under the works section.</h5>
+                            <h5>You can find out more about me on my About page!</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-        );
+        </div>
+
+    );
 }
 
 export default Home();
