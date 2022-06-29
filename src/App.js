63ad5ebbdc91ec82bab1544e6c90aead273afae6
Naomikho <khoyining9@gmail.com>
2022-06-29 22:53:58 +0800

    add menu to appbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from "./Home";
+import BasicMenu from './WorksMenu';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { HashRouter, Route, Switch, Link, Layout } from 'react-router-dom';
@@ -26,87 +27,70 @@ const darkTheme = createTheme({
   },
 });
 
-const workCategories = ['Games', 'Software & Systems', 'Art', 'Blogs & Writing'];
-
 const openURL = (url) => {
   window.open(url, "_self");
-}
-
-const ResponsiveAppBar = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 }
 
 function App() {
   return (
     <div className="App">
-    <ThemeProvider theme={darkTheme}>
-      
-      {/* app bar */}
-      <AppBar position="sticky">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-        <Box
-        component="img"
-        sx={{
-          height: 40,
-          width: 200,
-          pr: 72
-        }}
-        alt="Naomi Kho"
-        src={Name}/>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Button
-              key = {"Home"}
-              onClick={() => openURL('/')} 
-              sx={{ my: 2, color: 'white', display: 'block' , px: 8}}
-            >Home
-            </Button>
-            <Button
-              key = {"About"}
-              onClick={() => openURL('/About')} 
-              sx={{ my: 2, color: 'white', display: 'block' , px: 8}}
-            >About
-            </Button>
-            <Button
-              key = {"Works"}
-              onClick={() => openURL('/Games')} 
-              sx={{ my: 2, color: 'white', display: 'block' , px: 8}}
-            >Works
-            </Button>
-            <Button
-              key = {"Socials"}
-              onClick={() => openURL('/Socials')} 
-              sx={{ my: 2, color: 'white', display: 'block' , px: 8}}
-            >Socials
-            </Button>
-          </Box>
-        </Toolbar>
-      </Container>
+      <ThemeProvider theme={darkTheme}>
+
+        {/* app bar */}
+        <AppBar position="sticky">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box
+                component="img"
+                sx={{
+                  height: 40,
+                  width: 200,
+                  pr: 72
+                }}
+                alt="Naomi Kho"
+                src={Name} />
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Button
+                  key={"Home"}
+                  onClick={() => openURL('/')}
+                  sx={{ my: 2, color: 'white', display: 'block', px: 8 }}
+                >Home
+                </Button>
+                <Button
+                  key={"About"}
+                  onClick={() => openURL('/About')}
+                  sx={{ my: 2, color: 'white', display: 'block', px: 8 }}
+                >About
+                </Button>
+                
+                <BasicMenu></BasicMenu>
+
+                <Button
+                  key={"Socials"}
+                  onClick={() => openURL('/Socials')}
+                  sx={{ my: 2, color: 'white', display: 'block', px: 8 }}
+                >Socials
+                </Button>
+              </Box>
+            </Toolbar>
+          </Container>
         </AppBar>
 
-      Website is currently still a work in progress.
+        Website is currently still a work in progress.
 
-      <HashRouter basename={process.env.PUBLIC_URL}>
-             <Switch>
-                <Route path="/" element={Home} exact />
-                <Route path="/About" element={Home} exact />
-                <Route path="/Games" element={Home} exact />
-                <Route path="/Software" element={Home} exact />
-                <Route path="/Art" element={Home} exact />
-                <Route path="/Writing" element={Home} exact />
-                <Route path="/Socials" element={Home} exact />
-            </Switch>
-    </HashRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/" element={Home} exact />
+            <Route path="/About" element={Home} exact />
+            <Route path="/Games" element={Home} exact />
+            <Route path="/Software" element={Home} exact />
+            <Route path="/Art" element={Home} exact />
+            <Route path="/Writing" element={Home} exact />
+            <Route path="/Socials" element={Home} exact />
+          </Switch>
+        </HashRouter>
 
-    </ThemeProvider>
+      </ThemeProvider>
     </div>
   );
 }
