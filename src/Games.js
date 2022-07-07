d40ef543421a757752f8b7ce65d808f09e255546
Naomikho <khoyining9@gmail.com>
2022-07-07 18:55:02 +0800

    update desc

--- src/Games.js
+++ src/Games.js
@@ -11,7 +11,7 @@ function Games(){
             <br></br>
             <br></br>
             <h2>Projects I Participated in</h2>
-            <PortfolioCard iconName = "KanojoStories" title = 'Kanojo Stories' role = 'Main Porgrammer' tools = 'Unity' 
+            <PortfolioCard iconName = "KanojoStories" title = 'Kanojo Stories' role = 'Main Porgrammer' tools = 'Unity, C#' 
             link = 'https://twitter.com/choco_mame_jp'
             desc = {'Kanojo Stories is a mobile dating sim that is currently being developed by チョコマメ.' +
              'As the programmer, I have created my own dialogue & choice import system as well as making the base game system.'}></PortfolioCard>
