734437ab47a4aeb9063765e0753974f2c002c558
Naomikho <khoyining9@gmail.com>
2022-07-20 15:56:08 +0800

    added photo to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import Photo from '../Assets/profile.jpg';
+import Box from '@mui/material/Box';
+
+const commonStyles = {
+    bgcolor: 'background.paper',
+    m: 1,
+    border: 1,
+    width: '160px',
+    height: '150px',
+  };
 
 function Home() {
     return (
@@ -7,6 +17,7 @@ function Home() {
                 <div class="middle">
                     <div class="inner">
                         <div className="textAlignCenter" centered>
+                               <img src = {Photo} className = 'photo'></img>
                             <h1>Hi, I'm Naomi, a developer and creator.</h1>
                             <br></br>
                             <h4>You can find my works and portfolio under the works section.</h4>
