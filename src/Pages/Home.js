77e408384f2c7d7de37a09b73ac60970c90b8507
Naomikho <khoyining9@gmail.com>
2022-07-21 21:35:59 +0800

    update home page design

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,20 +19,23 @@ function Home() {
             <br></br><br></br><br></br><br></br>
             <br></br><br></br>
             <div className="row">
-                <div className="column">
+                <div className="col35">
                     <div className="textAlignCenter" centered>
                         <img src={Photo} className='photo'></img>
-                        <h2 className = "Livvic">Hi, I'm Naomi!</h2>
+                        <h2 className = "Livvic"><b>Hi, I'm Naomi!</b></h2>
                         <h3 className = "Livvic">I am a developer and creator.</h3>
 
                     </div>
                 </div>
-                <div className="column">
+                <div className="col65">
                     <div className="textAlignJustify" centered>
-                    <h4 className = "Livvic">Please use the menu to navigate between the pages!</h4>
-                    <h4 className = "Livvic">My works and list of projects are listed under the works section.</h4>
-                    <h4 className = "Livvic">You can also read more about me on my About page!</h4>
-                    <h4 className = "Livvic">You can find the source code of this webpage on my <a className = "ulLink" onClick={() => { openURL('https://github.com/Naomikho', true) }}>GitHub.</a></h4>
+                        <div className = "paper">
+                        <h4 className = "Livvic">Welcome to my personal website! This is a website I created to as a portfolio.</h4>
+                        <h4 className = "Livvic">Please use the menu to navigate between the pages!</h4>
+                        <h4 className = "Livvic">My works and list of projects are listed under the works section.</h4>
+                        <h4 className = "Livvic">You can also read more about me on my About page!</h4>
+                        <h4 className = "Livvic">You can find the source code of this webpage on my <a className = "ulLink" onClick={() => { openURL('https://github.com/Naomikho', true) }}>GitHub.</a></h4>
+                        </div>
                     </div>
                 </div>
             </div>
